6d0402fd08a76672449ac4c5fabc33596e42e768
Stefanie <stefaniekheinrich@gmail.com>
2023-04-15 19:43:24 -0300

    atualizações validação user + popups

---
+++ API-2023.1-all_doneCARLOS/server/index.js
@@ -0,0 +1,111 @@
+const express = require("express");
+const app = express();
+const { Pool } = require("pg");
+const cors = require("cors")
+const bcrypt = require("bcrypt")
+
+const cliente = new Pool({
+    host: "localhost",
+    user: "postgres",
+    password: "1234",
+    database: "visiona"
+})
+function buscaUsuario(login, senha, res){
+    cliente.query("SELECT * FROM usuarios WHERE login = '"+login+"';", (err, result) => {
+        if(err){
+            console.log('erro query:',err);
+        }
+        if(result.rows.length === 1){
+        const senhaD = result.rows.values().next().value.senha
+        bcrypt.compare(senha, senhaD, function(err, result){
+            if(result){
+                res.send({msg: "Usuário logado com sucesso"})
+            }else{
+                res.send({msg: "Usuário não encontrado"})
+            }
+        })}else{
+            res.send({msg: "Usuário não encontrado"})
+        }
+})
+}
+
+async function existeUser(login) {
+    return new Promise((resolve, reject) => {
+      cliente.query("SELECT * FROM usuarios WHERE login = '"+login+"';", (err, result) => {
+        if (err) {
+          console.log('erro query:',err);
+          reject(err);
+        }
+        else {
+          resolve(result.rowCount > 0);
+        }
+      });
+    });
+  }
+
+function setUsuario(login, nome, senha, email, status, usuariotipo, datacadastro, res){
+    bcrypt.hash(senha, 10, (err, hash) => {
+        if (err){
+            res.send(err)
+        }else{
+    cliente.query(('insert into usuarios ("login", "nome", "senha", "email", "status", "usuariotipo", "datacadastro") values ('+"'"+login+"', '"+nome+"', '"+ hash + "', '"+email+"', '"+status+"', '"+usuariotipo+"', '" +datacadastro+"');"), (err, result)=> {
+        if(err){
+            console.log('erro SQL', err);
+        }//else{
+        //    //res.send({msg: "Usuário cadastrado com sucesso"})
+        // };
+
+    })}
+})}
+
+
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/login", (req, res) => {
+    const login  = req.body.login
+    const password  = req.body.password
+
+    buscaUsuario(login, password, res)
+    
+    })
+
+
+app.post("/cadastro", async(req, res)=>{
+    const {firstname} = req.body;
+    const {lastname} = req.body
+    const {login} = req.body;
+    const {email} = req.body;
+    const {password} = req.body;
+    const {date} = req.body;
+    const status = "ativo"
+    const usuarioTipo = "ADM"
+
+    const existee = await existeUser(login);
+
+    if(existee === false){
+        setUsuario(login, firstname +" "+ lastname, password, email, status, usuarioTipo, date, res )
+        res.send({msg: "Usuário cadastrado com sucesso."})
+    }
+    else{
+        res.send({msg: "Usuário ja cadastrado."})
+    }
+})
+
+app.get("/getInfo", (req, res)=>{
+
+    cliente.query('select login, status, usuarioTipo, dataCadastro from usuarios', (err, result)=>{
+        if(err) console.log(err);
+        else res.json(result.rows);
+    })
+
+})
+
+app.listen(3001, () => {
+    console.log("rodando servidor");
+});
+
+
+
+
---
+++ API-2023.1-all_doneCARLOS/src/Pages/Form.js
@@ -0,0 +1,192 @@
+import React, { useState } from "react";
+import './CSS/form.css'
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Swal from 'sweetalert2'
+//Swal = require('sweetalert2')
+
+export function Form() {
+    const [values, setValues] = useState();
+    const navigate = useNavigate();
+    const handleChangeValues = (value)=>{
+      document.getElementById('btnCadastro').disabled = true
+      
+      setValues(prevValue=>({
+        ...prevValue,
+        [value.target.name]: value.target.value,
+      }))
+      desabilitaCadastro()
+    };
+
+    function desabilitaCadastro(){
+      if(validaVazio() === true || validaSenha() === false){
+        document.getElementById('btnCadastro').disabled = true
+        document.getElementById('btnCadastro').style.background = 'rgb(250, 186, 75)'
+        document.getElementById('btnCadastro').style.cursor = 'not-allowed'
+      }else{
+        document.getElementById('btnCadastro').disabled = false
+        document.getElementById('btnCadastro').style.background = 'rgb(255, 165, 0)'
+        document.getElementById('btnCadastro').style.cursor = 'pointer'
+      }
+    }
+    
+    const validaVazio = () => {
+      let isVazio = false
+      if(document.getElementById('obg1').value === ''){
+        isVazio = true
+        return isVazio
+      }
+      if(document.getElementById('obg2').value === ''){
+        isVazio = true
+        return isVazio
+      }
+      if(document.getElementById('obg3').value === ''){
+        isVazio = true
+        return isVazio
+      }
+      if(document.getElementById('obg4').value === ''){
+        isVazio = true
+        return isVazio
+      }
+      if(document.getElementById('obg5').value === ''){
+        isVazio = true
+        return isVazio
+      }
+      if(document.getElementById('obg6').value === ''){
+        isVazio = true
+        return isVazio
+      }
+    }
+
+    const limpaCampos = ()=>{
+      document.getElementById('obg1').value = ''
+      document.getElementById('obg2').value = ''
+      document.getElementById('obg3').value = ''
+      document.getElementById('obg4').value = ''
+      document.getElementById('obg5').value = ''
+      document.getElementById('obg6').value = ''
+    }
+
+    const validaSenha = ()=>{
+      if (document.getElementById('obg5').value !== document.getElementById('obg6').value){
+        let isIgual = false
+        return isIgual
+      }else{
+        let isIgual = true
+        return isIgual
+      }
+
+    }
+
+    const handleClickButton = () =>{
+      
+        desabilitaCadastro()
+        const date = new Date().toLocaleString();
+        axios.post("http://localhost:3001/cadastro", {
+          login: values.login,
+          firstname: values.firstname,
+          lastname: values.lastname,
+          email: values.email,
+          password: values.password,
+          date: date
+        }).then((response)=>{
+          if (response.data.msg === 'Usuario ja cadastrado.'){
+            Swal.fire({
+              icon: 'error',
+              title: 'Erro',
+              text: response.data.msg,
+            })
+          }
+          if (response.data.msg === 'Usuário cadastrado com sucesso.'){
+          limpaCampos()
+          Swal.fire(
+            'Sucesso!',
+            response.data.msg,
+            'success'
+          )
+          }
+        });
+        }
+
+    return (
+      
+      <div>
+        <div className="cabecalho2">
+          <div className="container_linha2">
+            <p className="cad_titulo2">Cadastre-se</p>
+          </div>
+        </div>
+        <form className="form2">
+          <div className="container2">
+            <p className="sub_titulo2">
+              Preencha os campos abaixo com seus dados:
+            </p>
+            <div className="row2">
+              <div className="column2">
+              <input id="obg1"
+                  type="login"
+                  name="login"
+                  className="login--input"
+                  onChange={handleChangeValues}
+                  placeholder="Login"
+                />
+                <input id="obg2"
+                  type="firstname"
+                  name="firstname"
+                  className="fname--input"
+                  onChange={handleChangeValues}
+                  placeholder="Nome"
+                />
+                <input id="obg3"
+                  type="lastname"
+                  name="lastname"
+                  className="lname--input"
+                  onChange={handleChangeValues}
+                  placeholder="Sobrenome"
+                />
+              </div>
+              <div className="column2">
+                <input
+                  id="obg4"
+                  type="email"
+                  name="email"
+                  className="email--input"
+                  onChange={handleChangeValues}
+                  placeholder="E-mail"
+                />
+                <input
+                  id="obg5"
+                  type="password"
+                  name="password"
+                  className="name--input"
+                  onChange={handleChangeValues}
+                  placeholder="Senha"
+                />
+                <input  id="obg6"
+                  type="password"
+                  name="confirmpassword"
+                  className="confirmpassword--input"
+                  onChange={handleChangeValues}
+                  placeholder="Confirmar Senha"
+                />
+              </div>
+              {/* <div className="conteiner_termo2">
+                <div className="termosAcesso2">
+                  <input type="checkbox" className="checkbox2" id="checkbox"/>
+                  <label for="checkbox" className="acesso_txt2">
+                    Li e aceito os termos de acesso
+                  </label>
+                </div>
+              </div> */}
+            </div>
+          </div>
+
+          <div className="botoes2" onLoad='desabilitaCadastro()'>
+            <button onClick={()=>[navigate ('/')]}>voltar</button>
+            <button id = 'btnCadastro' type="button" className="register--button2" 
+            onClick={()=>handleClickButton()}> cadastrar </button>
+          </div>
+        </form>
+      </div>
+    );
+}
