fb25e78b1e3cfb68cf76d7e98dcd78be5bc6a3ee
Stefanie <stefaniekheinrich@gmail.com>
2023-04-16 09:18:03 -0300

    ativar/desativar user

--- API-2023.1-all_doneCARLOS/server/index.js
+++ API-2023.1-all_doneCARLOS/server/index.js
@@ -1,111 +1,194 @@
 const express = require("express");
 const app = express();
 const { Pool } = require("pg");
-const cors = require("cors")
-const bcrypt = require("bcrypt")
+const cors = require("cors");
+const bcrypt = require("bcrypt");
 
 const cliente = new Pool({
-    host: "localhost",
-    user: "postgres",
-    password: "1234",
-    database: "visiona"
-})
-function buscaUsuario(login, senha, res){
-    cliente.query("SELECT * FROM usuarios WHERE login = '"+login+"';", (err, result) => {
-        if(err){
-            console.log('erro query:',err);
-        }
-        if(result.rows.length === 1){
-        const senhaD = result.rows.values().next().value.senha
-        bcrypt.compare(senha, senhaD, function(err, result){
-            if(result){
-                res.send({msg: "Usuário logado com sucesso"})
-            }else{
-                res.send({msg: "Usuário não encontrado"})
-            }
-        })}else{
-            res.send({msg: "Usuário não encontrado"})
-        }
-})
+  host: "localhost",
+  user: "postgres",
+  password: "1234",
+  database: "visiona",
+});
+function buscaUsuario(login, senha, res) {
+  cliente.query(
+    "SELECT * FROM usuarios WHERE login = '" + login + "';",
+    (err, result) => {
+      if (err) {
+        console.log("erro query:", err);
+      }
+      if (result.rows.length === 1) {
+        const senhaD = result.rows.values().next().value.senha;
+        bcrypt.compare(senha, senhaD, function(err, result) {
+          if (result) {
+            res.send({ msg: "Usuário logado com sucesso" });
+          } else {
+            res.send({ msg: "Usuário não encontrado" });
+          }
+        });
+      } else {
+        res.send({ msg: "Usuário não encontrado" });
+      }
+    }
+  );
 }
 
 async function existeUser(login) {
-    return new Promise((resolve, reject) => {
-      cliente.query("SELECT * FROM usuarios WHERE login = '"+login+"';", (err, result) => {
+  return new Promise((resolve, reject) => {
+    cliente.query(
+      "SELECT * FROM usuarios WHERE login = '" + login + "';",
+      (err, result) => {
         if (err) {
-          console.log('erro query:',err);
+          console.log("erro query:", err);
           reject(err);
-        }
-        else {
+        } else {
           resolve(result.rowCount > 0);
         }
-      });
-    });
-  }
+      }
+    );
+  });
+}
 
-function setUsuario(login, nome, senha, email, status, usuariotipo, datacadastro, res){
-    bcrypt.hash(senha, 10, (err, hash) => {
-        if (err){
-            res.send(err)
-        }else{
-    cliente.query(('insert into usuarios ("login", "nome", "senha", "email", "status", "usuariotipo", "datacadastro") values ('+"'"+login+"', '"+nome+"', '"+ hash + "', '"+email+"', '"+status+"', '"+usuariotipo+"', '" +datacadastro+"');"), (err, result)=> {
-        if(err){
-            console.log('erro SQL', err);
-        }//else{
-        //    //res.send({msg: "Usuário cadastrado com sucesso"})
-        // };
-
-    })}
-})}
-
-
+function setUsuario(
+  login,
+  nome,
+  senha,
+  email,
+  status,
+  usuariotipo,
+  datacadastro,
+  res
+) {
+  bcrypt.hash(senha, 10, (err, hash) => {
+    if (err) {
+      res.send(err);
+    } else {
+      cliente.query(
+        'insert into usuarios ("login", "nome", "senha", "email", "status", "usuariotipo", "datacadastro") values (' +
+          "'" +
+          login +
+          "', '" +
+          nome +
+          "', '" +
+          hash +
+          "', '" +
+          email +
+          "', '" +
+          status +
+          "', '" +
+          usuariotipo +
+          "', '" +
+          datacadastro +
+          "');",
+        (err, result) => {
+          if (err) {
+            console.log("erro SQL", err);
+          } //else{
+          //    //res.send({msg: "Usuário cadastrado com sucesso"})
+          // };
+        }
+      );
+    }
+  });
+}
 
 app.use(cors());
 app.use(express.json());
 
 app.post("/login", (req, res) => {
-    const login  = req.body.login
-    const password  = req.body.password
+  const login = req.body.login;
+  const password = req.body.password;
 
-    buscaUsuario(login, password, res)
-    
-    })
-
-
-app.post("/cadastro", async(req, res)=>{
-    const {firstname} = req.body;
-    const {lastname} = req.body
-    const {login} = req.body;
-    const {email} = req.body;
-    const {password} = req.body;
-    const {date} = req.body;
-    const status = "ativo"
-    const usuarioTipo = "ADM"
-
-    const existee = await existeUser(login);
-
-    if(existee === false){
-        setUsuario(login, firstname +" "+ lastname, password, email, status, usuarioTipo, date, res )
-        res.send({msg: "Usuário cadastrado com sucesso."})
-    }
-    else{
-        res.send({msg: "Usuário ja cadastrado."})
-    }
-})
-
-app.get("/getInfo", (req, res)=>{
-
-    cliente.query('select login, status, usuarioTipo, dataCadastro from usuarios', (err, result)=>{
-        if(err) console.log(err);
-        else res.json(result.rows);
-    })
-
-})
-
-app.listen(3001, () => {
-    console.log("rodando servidor");
+  buscaUsuario(login, password, res);
 });
 
+app.post("/cadastro", async (req, res) => {
+  const { firstname } = req.body;
+  const { lastname } = req.body;
+  const { login } = req.body;
+  const { email } = req.body;
+  const { password } = req.body;
+  const { date } = req.body;
+  const status = "ativo";
+  const usuarioTipo = "ADM";
 
+  const existee = await existeUser(login);
 
+  if (existee === false) {
+    setUsuario(
+      login,
+      firstname + " " + lastname,
+      password,
+      email,
+      status,
+      usuarioTipo,
+      date,
+      res
+    );
+    res.send({ msg: "Usuário cadastrado com sucesso." });
+  } else {
+    res.send({ msg: "Usuário ja cadastrado." });
+  }
+});
 
+app.get("/getInfo", (req, res) => {
+  cliente.query(
+    "select login, status, usuarioTipo, dataCadastro from usuarios ORDER BY dataCadastro DESC",
+    (err, result) => {
+      if (err) console.log(err);
+      else res.json(result.rows);
+    }
+  );
+});
+
+//ativar/desativar usuário
+app.delete("/usuarios/:login", (req, res) => {
+  const login = req.params.login;
+  cliente.query(
+    `SELECT status FROM usuarios WHERE login = $1`,
+    [login],
+    (err, result) => {
+      if (err) {
+        console.log("erro query:", err);
+        res.status(500).send({ msg: "Erro ao buscar usuário" });
+      } else {
+        if (result.rowCount === 0) {
+          res.status(404).send({ msg: "Usuário não encontrado" });
+        } else {
+          const status = result.rows[0].status;
+          if (status === "ativo") {
+            cliente.query(
+              `UPDATE usuarios SET status = 'inativo' WHERE login = $1`,
+              [login],
+              (err, result) => {
+                if (err) {
+                  console.log("erro query:", err);
+                  res.status(500).send({ msg: "Erro ao desativar usuário" });
+                } else {
+                  res.send({ msg: "Usuário desativado com sucesso" });
+                }
+              }
+            );
+          } else {
+            cliente.query(
+              `UPDATE usuarios SET status = 'ativo' WHERE login = $1`,
+              [login],
+              (err, result) => {
+                if (err) {
+                  console.log("erro query:", err);
+                  res.status(500).send({ msg: "Erro ao ativar usuário" });
+                } else {
+                  res.send({ msg: "Usuário ativado com sucesso" });
+                }
+              }
+            );
+          }
+        }
+      }
+    }
+  );
+});
+
+app.listen(3001, () => {
+  console.log("rodando servidor");
+});
--- API-2023.1-all_doneCARLOS/src/Pages/Tabela/tabela.js
+++ API-2023.1-all_doneCARLOS/src/Pages/Tabela/tabela.js
@@ -1,79 +1,106 @@
-
 import "./tabela.css";
 import Navbar from ".././../components/Navbar";
-import { MDBBadge, MDBBtn, MDBTable, MDBTableHead, MDBTableBody } from 'mdb-react-ui-kit';
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
+import {
+  MDBBadge,
+  MDBBtn,
+  MDBTable,
+  MDBTableHead,
+  MDBTableBody,
+} from "mdb-react-ui-kit";
 
-function Lista({users}) {
+
+function Lista({ users }) {
+
+  async function modifyStatus(item) {
+    fetch(`http://localhost:3001/usuarios/${item.login}`, {
+  method: "DELETE",
   
-  function statusValida(item){
-  if (item.status === "ativo") {
-    return 'success'
-  } else {
-    return 'danger'
-  }}
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error));
+  window.location.reload(false);}
 
+  function statusValida(item) {
+    if (item.status === "ativo") {
+      return "success";
+    } else {
+      return "danger";
+    }
+    
+  }
 
-
-  
   return (
     <>
-    <Navbar/>
-    <div className="container3">
-      <div className="titulo3">
-        <div className="title-container3">
-          <h1>Controle de usuários</h1>
-          <hr className="title-hr3"></hr>
+      <Navbar />
+      <div className="container3">
+        <div className="titulo3">
+          <div className="title-container3">
+            <h1>Controle de usuários</h1>
+            <hr className="title-hr3" />
+          </div>
+          <div />
         </div>
-        <div></div>
-      </div>
 
-      <div className="fundo3">
-      <MDBTable align='middle'>
-      <MDBTableHead>
-        <tr>
-          <th scope='col' className='fw-bold mb-1'>ㅤLogin</th>
-          <th scope='col' className='fw-bold mb-1'>ㅤㅤㅤCadstro</th>
-          <th scope='col' className='fw-bold mb-1'>Status</th>
-          <th scope='col' className='fw-bold mb-1'>ㅤTipo</th>
-          <th scope='col' className='fw-bold mb-1'>ㅤㅤㅤAções</th>
-        </tr>
-      </MDBTableHead>
-      <MDBTableBody >{users.map((item, i )=>(
-        <tr key={i}>
-          <td >
-            <div className='d-flex align-items-center'>
-              <div className='ms-3'>
-                <p className='fw-normal mb-1'>{item.login}</p>
-              </div>
-            </div>
-          </td>
-          <td>
-            <p className='fw-normal mb-1'>{item.datacadastro}</p>
-          </td>
-          <td>
-            <MDBBadge color={statusValida(item)} pill>
-            {item.status}
-            </MDBBadge>
-          </td>
-          <td>{item.usuariotipo}</td>
-          <td>
-            <MDBBtn color='link' rounded size='sm' >
-              Editar
-            </MDBBtn>
-            <MDBBtn  color="link" rounded size="sm">
-            Desativar                     
-            </MDBBtn>
-              
-          </td>
-        </tr>
-       ))}
-      </MDBTableBody> 
-    </MDBTable>
+        <div className="fundo3">
+          <MDBTable align="middle">
+            <MDBTableHead>
+              <tr>
+                <th scope="col" className="fw-bold mb-1">
+                  ㅤLogin
+                </th>
+                <th scope="col" className="fw-bold mb-1">
+                  ㅤㅤㅤCadstro
+                </th>
+                <th scope="col" className="fw-bold mb-1">
+                  Status
+                </th>
+                <th scope="col" className="fw-bold mb-1">
+                  ㅤTipo
+                </th>
+                <th scope="col" className="fw-bold mb-1">
+                  ㅤㅤㅤAções
+                </th>
+              </tr>
+            </MDBTableHead>
+            <MDBTableBody>
+              {users.map((item, i) => (
+                <tr key={i}>
+                  <td>
+                    <div className="d-flex align-items-center">
+                      <div className="ms-3">
+                        <p className="fw-normal mb-1">{item.login}</p>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    <p className="fw-normal mb-1">{item.datacadastro}</p>
+                  </td>
+                  <td>
+                    <MDBBadge color={statusValida(item)} pill>
+                      {item.status}
+                    </MDBBadge>
+                  </td>
+                  <td>{item.usuariotipo}</td>
+                  <td>
+                    <MDBBtn color="link" rounded size="sm" >
+                      Edit
+                    </MDBBtn>
+                    <MDBBtn
+                      color="link"
+                      rounded
+                      size="sm"
+                     onClick={() => modifyStatus(item)}
+                    >
+                      {item.status === "ativo" ? "Desativar" : "Ativar"}
+                    </MDBBtn>
+                  </td>
+                </tr>
+              ))}
+            </MDBTableBody>
+          </MDBTable>
 
-          
-              {/* {users.map((item, i )=>(
+          {/* {users.map((item, i )=>(
                   <tr key = {i} className="tr4">
                     <td className="td4Login"> {item.login} </td>
                     <td className="td4Data"> {item.datacadastro} </td>
@@ -81,11 +108,8 @@ function Lista({users}) {
                     <td className="td4Tipo"> {item.usuariotipo} </td>
                   </tr>
               ))} */}
-
-            
-        
+        </div>
       </div>
-    </div>
     </>
   );
 }
