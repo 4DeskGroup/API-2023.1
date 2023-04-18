feb90df0f2f5b3fcad0f56458f2296cd7263a707
Laiza Cristina Machado Zaic Truyts <laizacristinamt@gmail.com>
2023-04-17 21:50:22 -0300

    dashboard
    grafico

---
+++ API-2023.1_dashboard/src/routes/indexRoutes.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import { Form } from "../Pages/Form";
+//import Login from "../Pages/login";
+//import Welcome from "../Pages/Welcome";
+//import Table from "../Pages/Tabela/Table";
+import Dashboard from "../Pages/Dashboard";
+
+
+export const Rotas = () => {
+    return (
+        //Rotas da aplicação
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                {/*<Route path="/form" element={<Form />} />
+                <Route path="/welcome" element={<Welcome />} />
+                <Route path="/tabela" element={<Table />} />*/}
+            </Routes>
+        </BrowserRouter>
+    )
+}
