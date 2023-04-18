28311b4bc36ef173f4e47c656a9de1a925251948
Laiza Cristina Machado Zaic Truyts <laizacristinamt@gmail.com>
2023-04-18 10:27:04 -0300

    Update indexRoutes.js

--- API-2023.1_dashboard/src/routes/indexRoutes.js
+++ API-2023.1_dashboard/src/routes/indexRoutes.js
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-//import { Form } from "../Pages/Form";
-//import Login from "../Pages/login";
-//import Welcome from "../Pages/Welcome";
+import { Form } from "../Pages/Form";
+import Login from "../Pages/login";
+import Welcome from "../Pages/Welcome";
 import Table from "../Pages/Tabela/Table";
 import Dashboard from "../Pages/Dashboard";
+import { EditarPopup } from "../Pages/EditarPopup";
 
 
 
@@ -12,11 +13,12 @@ export const Rotas = () => {
         //Rotas da aplicação
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Dashboard />} />
-                {/*<Route path="/form" element={<Form />} />
+                <Route path="/" element={<Login />} />
+                <Route path="/form" element={<Form />} />
                 <Route path="/welcome" element={<Welcome />} />
-                <Route path="/tabela" element={<Table />} />*/}
-                <Route path="/tabela" element={<Table />}/>
+                <Route path="/tabela" element={<Table />} />
+                <Route path="/dashboard" element={<Dashboard />}/>
+                <Route path="/editarpopup" element={<EditarPopup />}/>
             </Routes>
         </BrowserRouter>
     )
