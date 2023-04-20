046df918eaad3c57e73dfd96d3ddcdcde0c59562
Rita Hecht <89950512+ritahecht@users.noreply.github.com>
2023-04-20 10:36:05 -0300

    Page Edição
    Adicionado paina ediçao

--- API-2023.1-all_doneCARLOS/src/components/BottomNavbar.js
+++ API-2023.1-all_doneCARLOS/src/components/BottomNavbar.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {
     MDBContainer,
     MDBNavbar,
+    MDBFooter,
     MDBNavbarBrand,
     MDBNavbarToggler,
     MDBIcon,
@@ -20,16 +21,18 @@ export default function BottomNavbar() {
     const [showBasic, setShowBasic] = useState(false);
 
     return (
-        <MDBNavbar className='fixed-bottom' expand='lg' light bgColor='light'>
-            <MDBContainer fluid>
-                <MDBNavbarBrand className='mx-4' href='#' style={{ color: '#FF6B00'}}><h6>4DESK</h6></MDBNavbarBrand>
+        <MDBFooter bgColor='light' className='text-center text-lg-left'>
+            <MDBContainer className='text-center text-md-start mt-5'>
 
-                <MDBNavbarNav className='mr-auto mb-2 mb-lg-0 d-flex justify-content-center'>
-                    <MDBNavbarItem className='mx-4' style={{ color: '#FF6B00'}}>
-                        <h6>@copyright - todos os direitos reservados - 2023</h6>
-                    </MDBNavbarItem>
-                </MDBNavbarNav>
-            </MDBContainer>
-        </MDBNavbar>
+              <MDBNavbar className='fixed-bottom' expand='lg' light bgColor='light'>
+
+                    <div className='text-center p-3' style={{ color: '#FF6B00' }}>
+                     &copy; {new Date().getFullYear()} Copyright:{' '}
+                 <a className='text-warning' href='#' style={{color: '#FF6B00'}}>4DESK
+                    </a>
+                        </div>
+                </MDBNavbar>     
+                </MDBContainer>
+                </MDBFooter>
     );
 }
